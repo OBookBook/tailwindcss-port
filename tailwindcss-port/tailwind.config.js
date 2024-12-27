c19f5c4e0cae38a0c5143f575b3891b6258d359d
N.I <iworksWeb@gmail.com>
2024-12-28 07:26:32 +0900

    add init  tailwind css

--- tailwindcss-port/tailwind.config.js
+++ tailwindcss-port/tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    // https://tailwindcss.com/docs/configuration
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        body: "#050505",
+        "selected-text": "red",
+        theme: "#5c318c",
+        secondary: "#9191A4",
+        badge: "#3F3F51",
+        inputBorder: "#565666",
+        input: "#2A2A33",
+        primary: {
+          100: "#ebf8ff",
+          300: "#90cdf4",
+          500: "#4299e1",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
